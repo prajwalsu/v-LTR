fad500fd9354b8461b60ae5ea7155c790e587e34
Prajwal Umapathy <107015@tfcorp.com>
2022-09-21 18:07:29 +0530

    LTR-files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Little Indian Restro</h1>
+  <ul>
+    <dish-details
+      v-for="dish in dishes"
+      :key="dish.id"
+      :name="dish.name"
+      :description="dish.description"
+      :price="dish.price"
+      :isFavourite="dish.isFavourite"
+      :id="dish.id"
+      @toggleFavouriteStatus="toggleFavouriteStatus"
+    />
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DishDetails from "./components/DishDetails.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { DishDetails },
+  data() {
+    return {
+      dishes: [
+        {
+          id: "avi",
+          name: "Butter Chicken 🍗",
+          description: "It's finger liking good",
+          price: "350",
+          isFavourite: true,
+        },
+        {
+          id: "vaibhav",
+          name: "Chole Bhature 🥗",
+          description:
+            " It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a deep-fried bread made from maida.",
+          price: "150",
+          isFavourite: false,
+        },
+        {
+          id: "akshata",
+          name: "Fish Fry 🎣",
+          description:
+            " fish fry is a meal containing battered or breaded fried fish.",
+          price: "300",
+          isFavourite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    //to get selected dishDetail from child component
+    toggleFavouriteStatus(dishID) {
+      // console.log("dishId in parent", dishID);
+      let identifiedDish = this.dishes.find((dish) => dish.id === dishID);
+      //console.log("identifiedDish",identifiedDish);
+      identifiedDish.isFavourite = !identifiedDish.isFavourite;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app h1{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+   background-color: blueviolet;
 }
 </style>
---
+++ src/components/DishDetails.vue
@@ -0,0 +1,76 @@
+<template>
+  <li>
+    <!-- <h2>{{ name }} {{ dishIsFavourite ? "(Favourite)" : "" }}</h2> -->
+    <h2>{{ isFavourite ? "⭐" : "" }} {{ name }} </h2>
+    <button @click="toggleFavourite">Toggle Favourite</button>
+    <button @click="toggleDetails">
+      {{ showDetails ? "Hide " : "Show " }}Details
+    </button>
+    <ul v-if="showDetails">
+      <li>
+        <strong>Description</strong>
+        {{ description }}
+      </li>
+      <li>
+        <strong>Price</strong>
+        {{ price }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  emits: ["toggleFavouriteStatus"],
+  //   props: ["name", "description", "price", "isFavourite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      //   dishIsFavourite: this.isFavourite,
+      showDetails: false,
+    };
+  },
+  methods: {
+    toggleFavourite() {
+      //   this.dishIsFavourite = !this.dishIsFavourite;
+
+      //   if (this.dishIsFavourite) {
+      //     this.dishIsFavourite = false;
+      //   } else {
+      //     this.dishIsFavourite = true;
+      //   }
+      this.$emit("toggleFavouriteStatus", this.id);
+    },
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  list-style-type: none;
+}
+</style>
